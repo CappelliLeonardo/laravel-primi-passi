51a840090bf12725a356f290db747b4da7bd33c7
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-02-22 15:15:59 +0100

    Add bonus

--- resources/views/AboutUs.blade.php
+++ resources/views/AboutUs.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Primi Passi</title>
+    <title>ABOUTUS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
     crossorigin="anonymous">
@@ -11,11 +11,11 @@
 <body>
     <header>
         <h1>
-            PRIMI GRANDI PICCOLI PASSI
+            ABOUTUS
         </h1>
     </header>
     <main>
-        <nav>
+    <nav>
             <ul>
                 <li>
                     <a class="nav-link" href="{{url('/')}}">Home</a>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/aboutUs', function () {
+    return view('aboutUS');
+});
